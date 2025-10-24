4ff7275cb474f58885943a4ad2107cc5dc2de543
Serge Pegoff <spegoff@authentica.eu>
2025-10-24 18:50:07 +0200

    mailer preview 'received' OK

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,16 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/received
-  def received(order)
-    @order = order
-
-    mail to: order.email, subject: "Pragmatic Store Order Confirmation"
+  def received
+    order = Order.first
+    OrderMailer.received(order)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/shipped
-  def shipped(order)
-    @order = order
-
-    mail to: order.email, subject: "Pragmatic Store Order Shipped"
+  def shipped
+    order = Order.first
+    OrderMailer.shipped(order)
   end
 end
